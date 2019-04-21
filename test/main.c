f1be9ac89c6bc7047df1076710268bbfd184ba8e
Stephen Collins <scrivera64@gmail.com>
2019-04-20 21:13:12 -0600

    test: add logging to indicate load time

--- test/main.c
+++ test/main.c
@@ -1,3 +1,6 @@
+#include <time.h>
+#include <stdio.h>
+
 #include "cmenu-tree.h"
 
 int
@@ -5,7 +8,10 @@ main ()
 {
     CMenuTree *tree;
     GError *error = NULL;
+    clock_t start = clock ();
 
     tree = cmenu_tree_new ("cinnamon-applications.menu", CMENU_TREE_FLAGS_INCLUDE_NODISPLAY);
     cmenu_tree_load_sync (tree, &error);
+
+    printf("Loaded in %f ms\n", ((double) (clock () - start)) * 1000 / CLOCKS_PER_SEC);
 }
